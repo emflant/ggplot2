d60241e8392ca674d9000a19542e3638ead47b9d
“emflant” <emflant@gmail.com>
2022-08-27 00:51:02 +0900

    .

--- 2022/20220824/ggplot2_20220824.R
+++ 2022/20220824/ggplot2_20220824.R
@@ -7,16 +7,24 @@ library(treemapify)
 
 v_background_color = "#2E5E77"
 v_background_color = "gray100"
-g1 = tibble(x = 1:5, y = c(75, 64, 114, 36, 38)) %>% 
+tibble(x = 1:5, y = c(75, 64, 114, 36, 38),
+       a = c(1,1,1,0,1)) %>% 
   mutate(z = y / 4 + 30) %>% 
   ggplot() + 
-  geom_col(aes(x,z, fill = factor(x)), width = 1, colour = "gray100", size = 1) +
+  geom_col(aes(x,z, fill = factor(x)), width = 1, colour = "gray100", size = 2) +
   geom_col(aes(x,y = 30), width = 1, fill = "gray100") +
-  # geom_label(aes(x, 25, label = x)) +
+  # geom_label(aes(x, y/10 + 30), label = LETTERS[1:5],
+  #            label.size = 0, family = "BMJUAOTF",
+  #            color = "black") +
+  geom_text(aes(x, y/10 + 30 + a), label = LETTERS[1:5],
+             family = "BMJUAOTF", size = 8,
+             color = "gray100") +
   coord_polar(theta = "x") +
-  scale_fill_brewer(palette = "Purples",
-                    direction = -1) +
-  theme_void() +
+  # scale_fill_brewer(palette = "Purples",
+  #                   direction = -1) +
+  scale_fill_manual(values = c("#9E9AC8", "#9E9AC8", "#53278F", "#9E9AC8", "#9E9AC8")) +
+  # theme_minimal() + #9E9AC8 #53278F
+  theme_void(base_family = "BMJUAOTF") +
   theme(legend.position = "none",
         plot.caption = element_text(color = "gray0", 
                                     family = "Menlo", 
@@ -25,8 +33,9 @@ g1 = tibble(x = 1:5, y = c(75, 64, 114, 36, 38)) %>%
                                     margin = margin(0.2,0,0,0,"cm")),
         # plot.margin = margin(0.5,0.5,0.3,0.4,"in"),
         # plot.margin = margin(0,0,0,0,"in"),
+        # axis.text.x = element_text(),
         plot.background = element_rect(fill = v_background_color, color = v_background_color))
-
+# g1
 ggsave("~/github/ggplot2/2022/20220824/save_ggplot_02.png", 
        width = 8, height = 6, dpi = 320, units = "in")
 # tibble(x = 1:5, y = c(30, 30, 30, 30, 30), 
@@ -73,29 +82,25 @@ tibble(w = c(1,5,2), y = rep(1,3)) %>%
 ##################################################
 v_background_color = "#2E5E77"
 v_background_color = "gray100"
-g2 = tibble(w = c(1,4),
+tibble(w = c(1,4),
        h = rep(1,2),
-       y = c(1.3,1)) %>% 
+       y = c(1.3,1),
+       a = c(0.03,-0.06)) %>% 
   mutate(w1 = cumsum(w)) %>% 
   mutate(w2 = lag(w1, 1, default = 0)) %>% 
   mutate(x = w / 2 + w2) %>% 
   ggplot(aes(x,y,
              width = w, height = h,
              fill = factor(x))) +
-  geom_tile(size = 2, colour = v_background_color) +
-  ylim(c(-2.5, 2)) +
+  geom_tile(size = 3, colour = v_background_color) +
+  geom_text(aes(x,y + a), label = LETTERS[1:2],
+            family = "BMJUAOTF", size = 8,
+            colour = "gray100") +
+  ylim(c(-2, 2)) +
   coord_polar(theta = "x") +
-  scale_fill_manual(values = c("#7070A2", "#9F9EBF")) +
-  # scale_fill_manual(values = c("#7b59b3", "#A67EE2")) +
+  scale_fill_manual(values = c("#53278F", "#9E9AC8")) +
   theme_void() +
-  # labs(caption = "@sourcebox7") +
   theme(legend.position = "none",
-        plot.caption = element_text(color = "gray0", 
-                                    family = "Menlo", 
-                                    hjust = 0.99, 
-                                    size = 10,
-                                    margin = margin(0,0,0,0,"cm")),
-        # plot.margin = margin(0.5,0.5,0.3,0.4,"in"),
         plot.background = element_rect(fill = v_background_color, color = v_background_color))
 
 ggsave("~/github/ggplot2/2022/20220824/save_ggplot_01.png", 
